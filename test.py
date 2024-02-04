15e3574f2ef4cd3486705f8c91f53f9f58ff5d54
Lobov Sergey <loboff09@gmail.com>
2024-02-04 23:02:24 +0300

    0/0/1

--- test.py
+++ test.py
@@ -1,15 +1,37 @@
-# from enum import unique
-# from sys import api_version
-# from traceback import print_list
-# import numpy as np
-# from itertools import permutations
-
-# TEST_CASE_1 = [1,2,3]
-# TEST_CASE_2 = [7,1,8]
+from collections import Counter
+from enum import unique
+from sys import api_version
+from traceback import print_list
+import numpy as np
+from itertools import permutations
 
 
-# a = list(permutations(TEST_CASE_1))
-# b = list(permutations(TEST_CASE_2))
+def find(x,y):
+    c = []
+    for i in x:
+        if i in y:
+            c.append(i)
+    return c
+
+
+def sort(a, lis):
+    
+
+TEST_CASE_1 = [1,2,3,8]
+TEST_CASE_2 = [7,1,8,1]
+
+A_T1 = 
+A_T2 = sorted(TEST_CASE_2)
+
+print(A_T1)
+print('-----------------------------')
+print(A_T2)
+print(np.equal(A_T1,A_T2))
+
+
+
+a = list(permutations(TEST_CASE_1))
+b = list(permutations(TEST_CASE_2))
 # ap = np.array(a)
 # print(ap)
 # print(len(ap))
@@ -17,8 +39,8 @@
 # print('-----------------------------')
 # print(bp)
 # print(len(bp))
-# uniqu = 0
-# u = 0
+uniqu = 0
+u = 0
 
 # for i in ap:
 #     for j in bp:
@@ -32,13 +54,6 @@
 # print(u)
 # print(uniqu/u)
          
-
-from math import factorial
-
-
-print(factorial(30))
-
-
 
 
 
