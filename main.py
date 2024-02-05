a365ec760c368f31ed89a039b9eda212119fbdcc
sergey <loboff09@gmail.com>
2024-02-05 15:16:08 +0300

    0/0/3

--- main.py
+++ main.py
@@ -1,75 +1,73 @@
 from itertools import permutations
 from math import factorial
-from random import randint
+from random import choice, randint, shuffle
 import numpy as np
 import time
 
-max_hours = 4
-max_teachers = 4
+max_hours = 5
+max_teachers = 5
 TEST_CASE = [[10, 8, 8], [6, 6, 8], [4, 6, 8]]
+# np.array(list(iteration.__next__()))
 
 
-def prepare_data():    
-    global len_permutation 
-    global lessons_list   
+def prepare_data():
+    global len_permutation
+    global lessons_list
     global head_combination
-    lessons_list = [[randint(0,8) for _ in range(max_hours)] for _ in range(max_teachers)]    
+    lessons_list = [[randint(0, 8) for _ in range(max_hours)]
+                    for _ in range(max_teachers)]
     # lessons_list = TEST_CASE
-    len_permutation = factorial(max_hours)    
+    len_permutation = factorial(max_hours)
     head_lessons = lessons_list[0]
     head_combination = permutations(head_lessons)
     # list_permutations = [permutations(teacher_class) for teacher_class in lessons_list]
-    
 
 
+def coefficients(item_1, item_2):
+    bool_list = np.equal(item_1, item_2)
 
 
-
-def head_call(): 
-    try:    
-        head = head_combination.__next__()          
-        return head   
-    except StopIteration:        
+def head_call():
+    try:
+        head = head_combination.__next__()
+        return head
+    except StopIteration:
         return None
-    
- 
 
 
 def combinate(head_combination, *lessons):
-        print('ПРИШОЛ')                         
-        unpacked_lessons = lessons[0]
-        lesson = unpacked_lessons[0] 
-        iteration = permutations(lesson)            
-        for _ in range(int(len_permutation / 4)):            
-            candidate_row = np.array(list(iteration.__next__()))                
-            res_bool = np.equal(head_combination, candidate_row) 
-            print(res_bool)
-            unique_bool = np.unique(res_bool)
-            if unique_bool.size == 1 and not unique_bool[0]:
-                finded_combination = np.vstack([head_combination, candidate_row])                
-                if len(unpacked_lessons) != 1:
-                    result = combinate(finded_combination, unpacked_lessons[1:])     
-                    if type(result) == tuple:
-                        finded_combination = finded_combination[:-1:]                         
-                        continue 
-                    print('УШОЛ')   
-                    return result  
-                print('УШОЛ')                                        
-                return finded_combination
-        print('УШОЛ')  
-        return (head_combination, 0)
-            
-def main():                
-    while True:  
+    print('ПРИШОЛ')
+    unpacked_lessons = lessons[0]
+    lesson = unpacked_lessons[0]
+    iteration = list(permutations(lesson))
+    for _ in range(len_permutation):
+        candidate_row = choice(iteration)
+        res_bool = np.equal(head_combination, candidate_row)
+        unique_bool = np.unique(res_bool)
+        if unique_bool.size == 1 and not unique_bool[0]:
+            finded_combination = np.vstack([head_combination, candidate_row])
+            if len(unpacked_lessons) != 1:
+                result = combinate(finded_combination, unpacked_lessons[1:])
+                if type(result) == tuple:
+                    finded_combination = finded_combination[:-1:]
+                    continue
+                print('УШОЛ')
+                return result
+            print('УШОЛ')
+            return finded_combination
+    print('УШОЛ')
+    return (head_combination, 0)
+
+
+def main():
+    while True:
         head = head_call()
         if head is None:
-            return f"No combination! {lessons_list}"    
+            return f"No combination! {lessons_list}"
         main_combination = combinate(head, lessons_list[1:])
-        if type(main_combination) == tuple:            
+        if type(main_combination) == tuple:
             continue
         return main_combination
-    
-
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,78 +1,31 @@
 from collections import Counter
 from enum import unique
+from random import choice, randint
 from sys import api_version
 from traceback import print_list
 import numpy as np
 from itertools import permutations
 
+max_hours = 5
+max_teachers = 3
+l = [[randint(0, 8) for _ in range(max_hours)] for _ in range(max_teachers)]
 
-def find(x,y):
-    c = []
-    for i in x:
-        if i in y:
-            c.append(i)
-    return c
-
-
-def sort(a, lis):
-    
-
-TEST_CASE_1 = [1,2,3,8]
-TEST_CASE_2 = [7,1,8,1]
-
-A_T1 = 
-A_T2 = sorted(TEST_CASE_2)
-
-print(A_T1)
-print('-----------------------------')
-print(A_T2)
-print(np.equal(A_T1,A_T2))
+s = np.array(l)
+s.sort()
 
 
+def sort_matrix(matr):
+    coloumn_counter = np.shape(matr)[0]
+    r_i = randint(0, coloumn_counter - 1)
+    l_s = np.array([[x] for x in matr if x[0] < matr[r_i][0]])
+    e_s = np.array([[x] for x in matr if x[0] == matr[r_i][0]])
+    g_s = np.array([[x] for x in matr if x[0] > matr[r_i][0]])
+    print(l_s)
+    print(e_s)
+    print(g_s)
 
-a = list(permutations(TEST_CASE_1))
-b = list(permutations(TEST_CASE_2))
-# ap = np.array(a)
-# print(ap)
-# print(len(ap))
-# bp = np.array(b)
-# print('-----------------------------')
-# print(bp)
-# print(len(bp))
-uniqu = 0
-u = 0
-
-# for i in ap:
-#     for j in bp:
-#         res = np.equal(i,j)
-#         res_b = np.unique(res)
-#         if res_b.size == 1 and not res_b[0]:
-#             uniqu += 1
-#         u += 1
-
-# print(uniqu)
-# print(u)
-# print(uniqu/u)
-         
+    # return np.vstack([sort_matrix(l_s), e_s, sort_matrix(g_s)])
 
 
-
-
-
-
-
-
-
-
-
-
-        
-        
-
-
-
-
-
-
-
-
+res = sort_matrix(s)
+print(res)
