b51e9e921d9d187a97dbc4054a6ffd0ca9fe4b85
sergey <loboff09@gmail.com>
2024-02-06 15:08:41 +0300

    00

--- test.py
+++ test.py
@@ -1,46 +1,54 @@
-from random import  randint
+from random import randint
 import numpy as np
 from itertools import permutations
 
 max_hours = 5
 max_teachers = 3
-l = [[randint(0, 9) for _ in range(max_hours)] for _ in range(max_teachers)]
-
-s = np.array(l)
-s.sort()
-print(bool([]))
-
-# def check(*args):
-#     for i in args[0]:
-#         if i
+t = 0
+f = 0
+log = ""
 
 
-
-def sort_matrix(matr, index=0): 
+def sort_matrix(matr, index=0):
     print("START WORK WITH", matr)
-    query = []   
-    rows = np.shape(matr)[0]    
+    rows = np.shape(matr)[0]
     r_i = randint(0, rows - 1)
-    l_s = np.array([x for x in matr if x[0] < matr[r_i][0]])
-    e_s = np.array([x for x in matr if x[0] == matr[r_i][0]])
-    g_s = np.array([x for x in matr if x[0] > matr[r_i][0]])
+    l_s = [x for x in matr if x[index] < matr[r_i][index]]
+    e_s = [x for x in matr if x[index] == matr[r_i][index]]
+    g_s = [x for x in matr if x[index] > matr[r_i][index]]
     print("<", l_s)
-    print("=",e_s)
-    print(">",g_s)
-    grades = [l_s, e_s, g_s]    
-    for i in grades:
-        if len(i) > 1:
-            if index > 5:
-                query.append(i)
-                print('ДОБАВИЛ ', i)
+    print("=", e_s)
+    print(">", g_s)
+    grades = [l_s, e_s, g_s]
+    print(grades)
+    for val in grades:
+        if len(val) > 1:
+            if np.equal(*val)[index]:
+                sort_matrix(val, index + 1)
             else:
-                sort_matrix(i, index + 1)
-        elif len(i) == 1:            
-            query.append(i)
-            print('ДОБАВИЛ ', i)
+                sort_matrix(val, index)
+        elif len(val) == 1:
+            query.append(val)
     return query
 
 
-res = sort_matrix(s)
-print(res)
+for _ in range(100):
+    l = [[randint(0, 9) for _ in range(max_hours)]
+         for _ in range(max_teachers)]
+    print("START", l)
+    l.sort()
+    print("SORT", l)
 
+    query = []
+    res = sort_matrix(l)
+    print(f"\033[32m{res}\033[0m")
+    if len(res) == 3:
+        t += 1
+    else:
+        f += 1
+        log += f"\n\n{res} \nWITH START\n {l}"
+
+print("TRUE", t)
+print("FALSE", f)
+input()
+print(log)
