4758d17fb6e0bb9ffd056ee0097ce73607c28989
Lobov Sergey <loboff09@gmail.com>
2024-02-06 22:59:42 +0300

    0/1/0

--- test.py
+++ test.py
@@ -1,54 +1,56 @@
 from random import randint
+from re import I
 import numpy as np
 from itertools import permutations
 
-max_hours = 5
-max_teachers = 3
-t = 0
-f = 0
-log = ""
+max_hours = 6
+max_teachers = 2
+
+def search(el, _list):
+    l_list = len(_list)    
+    if l_list == 1:
+        if el == _list[0]:
+            return True
+        else:
+            return False
+    i = len(_list) // 2        
+    if el in _list[0:i]:
+        return search(el, _list[:i])
+    return search(el, _list[i:])
+
+def chance_combination(l1,l2):    
+    count = 0
+    for i in l1:
+        if search(i, l2):
+            count += 1 
 
 
-def sort_matrix(matr, index=0):
-    print("START WORK WITH", matr)
-    rows = np.shape(matr)[0]
-    r_i = randint(0, rows - 1)
-    l_s = [x for x in matr if x[index] < matr[r_i][index]]
-    e_s = [x for x in matr if x[index] == matr[r_i][index]]
-    g_s = [x for x in matr if x[index] > matr[r_i][index]]
-    print("<", l_s)
-    print("=", e_s)
-    print(">", g_s)
-    grades = [l_s, e_s, g_s]
-    print(grades)
-    for val in grades:
-        if len(val) > 1:
-            if np.equal(*val)[index]:
-                sort_matrix(val, index + 1)
-            else:
-                sort_matrix(val, index)
-        elif len(val) == 1:
-            query.append(val)
-    return query
 
 
-for _ in range(100):
-    l = [[randint(0, 9) for _ in range(max_hours)]
-         for _ in range(max_teachers)]
-    print("START", l)
-    l.sort()
-    print("SORT", l)
+for _ in range(1):
+    # l = [[randint(0, 9) for _ in range(max_hours)]
+    #      for _ in range(max_teachers)]  
+    # l = [["1b","5c", "2b", "6c","1b", "None"], ["2b","3c", "1b", "5c","4b", "None"] ]  
+    # jll = list(map(lambda x : sorted(x), l))    
+    # l = np.array([*jll])
+    l1 = [1,2,3,4] 
+    l2 = [1,4,1,4]
+    l1_p = list(permutations(l1))
+    l2_p = list(permutations(l2))
+    uni = 0
+    all = 0
+    for i in l1_p:
+        for j in l2_p:
+            b_l = np.equal(i,j)
+            r = np.unique(b_l)
+            if len(r) == 1 and not r[0]:
+                uni += 1
+            all += 1
+    print("res", uni)
+    print("ALL", all)
+    print(uni/all)
 
-    query = []
-    res = sort_matrix(l)
-    print(f"\033[32m{res}\033[0m")
-    if len(res) == 3:
-        t += 1
-    else:
-        f += 1
-        log += f"\n\n{res} \nWITH START\n {l}"
 
-print("TRUE", t)
-print("FALSE", f)
-input()
-print(log)
+    
+    
+
