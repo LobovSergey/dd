60979388b6878d6395f5d18ff02fa6e372f31a56
sergey <loboff09@gmail.com>
2024-02-07 17:28:56 +0300

    0

--- test.py
+++ test.py
@@ -1,42 +1,43 @@
 from random import randint
 from re import I
 import numpy as np
+import pandas as pd
 from itertools import permutations
 
 max_hours = 6
 max_teachers = 2
 
+
 def search(el, _list):
-    l_list = len(_list)    
+    l_list = len(_list)
     if l_list == 1:
         if el == _list[0]:
             return True
         else:
             return False
-    i = len(_list) // 2        
+    i = len(_list) // 2
     if el in _list[0:i]:
         return search(el, _list[:i])
     return search(el, _list[i:])
 
-def chance_combination(l1,l2):    
+
+def chance_combination(l1, l2):
     count = 0
     for i in l1:
         if search(i, l2):
-            count += 1 
-
-
+            count += 1
 
 
 for _ in range(1):
     # l = [[randint(0, 9) for _ in range(max_hours)]
-    #      for _ in range(max_teachers)]  
-    # l = [["1b","5c", "2b", "6c","1b", "None"], ["2b","3c", "1b", "5c","4b", "None"] ]  
-    # jll = list(map(lambda x : sorted(x), l))    
+    #      for _ in range(max_teachers)]
+    # l = [["1b","5c", "2b", "6c","1b", "None"], ["2b","3c", "1b", "5c","4b", "None"] ]
+    # jll = list(map(lambda x : sorted(x), l))
     # l = np.array([*jll])
-    l1 = [1,1,2,4,5,6,7,8,2,1,8] 
-    l2 = [1,4,1,4]
+    l1 = [1, 3, 2, 4]
+    l2 = [1, 4, 1, 4]
     l1_p = list(permutations(l1))
-    
+
     print(len(set(l1)), "uniq")
     print(len(l1), "slots")
     print(len(l1_p), "all")
@@ -44,8 +45,6 @@ for _ in range(1):
     print(len(set(l1)) / len(l1), "K types")
     print(len(set(l1_p)) / len(l1_p), 'K combos')
 
-    
-    
     # uni = 0
     # all = 0
     # for i in l1_p:
@@ -58,8 +57,3 @@ for _ in range(1):
     # print("res", uni)
     # print("ALL", all)
     # print(uni/all)
-
-
-    
-    
-
